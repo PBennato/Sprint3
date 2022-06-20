ff8e7af98085a9ba81e171fcb1a53f48e96cd33a
gianni999 <gianniliotti2000@gmail.com>
2022-06-20 12:31:14 -0300

    Final

--- script.js
+++ script.js
@@ -1,57 +1,10 @@
-let otroObjeto = {
-    "compra":"121,96",
-    "venta":"127,96",
-    "agencia":"349",
-    "nombre":"Dolar Oficial",
-    "variacion":"0,27",
-    "ventaCero":"TRUE",
-    "decimales":"2"
-}
-
-let otroObjeto1 ={
-    "compra":"216,00",
-    "venta":"219,00",
-    "agencia":"310",
-    "nombre":"Dolar Blue",
-    "variacion":"-2,23",
-    "ventaCero":"TRUE",
-    "decimales":"2"
-}
-
-let otroObjeto2 = {
-    "nombre":"Dolar turista",
-    "compra":"No Cotiza",
-    "venta":"211,13",
-    "agencia":"406",
-    "variacion":"0,27",
-    "ventaCero":"TRUE",
-    "decimales":"2"
-}
-
-let otroObjeto3 = {
-    "compra":"237,79",
-    "venta":"238,14",
-    "agencia":"312",
-    "nombre":"Dolar Contado con Liqui",
-    "variacion":"-0,41",
-    "ventaCero":"TRUE",
-    "decimales":"2"
-}
-
-let otroObjeto4 = {
-    "compra":"229,450",
-    "venta":"229,080",
-    "agencia":"313",
-    "nombre":"Dolar Bolsa",
-    "variacion":"-1,500",
-    "ventaCero":"TRUE",
-    "decimales":"3"
-}
-
-let otroObjeto5 = {
-    "compra":"120,37",
-    "venta":"128,25",
-    "agencia":"302",
-    "nombre":"Dolar",
-    "decimales":"3"
-}
+function cargar() {
+    fetch("https://www.dolarsi.com/api/api.php?type=valoresprincipales")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data[0].casa.compra)
+        document.getElementById("doc").innerHTML=`compra:${data[0].casa.compra}`
+        document.getElementById("dov").innerHTML=`venta:${data[0].casa.venta}`
+        document.getElementById("vof").innerHTML=`variacion:${data[0].casa.variacion}`
+      });
+  }
